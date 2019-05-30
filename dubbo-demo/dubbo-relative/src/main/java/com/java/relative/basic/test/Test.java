e914e4fe6a0df69b39af7f939a643b6003bedbd9
陈胜勇 <csyGitHub>
2019-05-30 22:27:27 +0800

    native方式测试

---
+++ dubbo-demo/dubbo-relative/src/main/java/com/java/relative/basic/test/Test.java
@@ -0,0 +1,11 @@
+package com.java.relative.basic.test;
+
+/**
+ * @author chensy
+ * @date 2019-05-30 17:14
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(System.getProperties().get("java.library.path"));
+    }
+}
